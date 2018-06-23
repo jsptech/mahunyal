add1d417936acbc9f24c4dde4d4b0ce2673ae371
Laxman Singh <jsptechnologies@gmail.com>
2018-06-23 17:31:03 +0545

    Initial commit

---
+++ includes/midheader.php
@@ -0,0 +1,33 @@
+<div class="header-middle p-0 bg-lightest xs-text-center">
+      <div class="container pt-0 pb-0">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4 col-md-5">
+            <div class="widget no-border m-0">
+              <a class="menuzord-brand pull-left flip xs-pull-center mb-15" href="javascript:void(0)"><img src="images/logo-wide.png" alt=""></a>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4">
+            <div class="widget no-border pull-right sm-pull-none sm-text-center mt-10 mb-10 m-0">
+              <ul class="list-inline">
+                <li><i class="fa fa-phone-square text-theme-colored font-36 mt-5 sm-display-block"></i></li>
+                <li>
+                  <a href="#" class="font-12 text-gray text-uppercase">Call us today!</a>
+                  <h5 class="font-14 m-0"> +977-91-580110</h5>
+                </li>
+              </ul>
+            </div>
+          </div>  
+          <div class="col-xs-12 col-sm-4 col-md-3">
+            <div class="widget no-border pull-right sm-pull-none sm-text-center mt-10 mb-10 m-0">
+              <ul class="list-inline">
+                <li><i class="fa fa-clock-o text-theme-colored font-36 mt-5 sm-display-block"></i></li>
+                <li>
+                  <a href="#" class="font-12 text-gray text-uppercase">We are open!</a>
+                  <h5 class="font-13 text-black m-0"> Sun-Fri 9:00-16:00</h5>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
---
+++ includes/topheader.php
@@ -0,0 +1,33 @@
+<div class="header-top bg-theme-color-2 sm-text-center p-0">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="widget no-border m-0">
+              <ul class="list-inline font-13 sm-text-center mt-5">
+                <li>
+                  <a class="text-white" href="#"><i class="fa fa-phone-square"></i> 091-580110</a>
+                </li>
+                
+                <li>
+                  <a class="text-white" href="#"><i class="fa fa-envelope"></i> mail@mahunyalschool.edu.np</a>
+                </li>
+                             
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-8">            
+            <div class="widget no-border m-0 mr-15 pull-right flip sm-pull-none sm-text-center">
+              <ul class="styled-icons icon-circled icon-sm pull-right flip sm-pull-none sm-text-center mt-sm-15">
+                <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram text-white"></i></a></li>                
+                <li>
+                  <a class="text-white" href="#">Login</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
